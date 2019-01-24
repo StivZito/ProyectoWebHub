75f95dbd7d17621d3805964b5ffdfdfcf6ebd0f5
StivZito <zito351@gmail.com>
2019-01-23 22:06:36 -0500

    ll

--- homepage.php
+++ homepage.php
@@ -32,13 +32,13 @@
 				   		</div>
 			   		</div>
 			   	</li>
-			   	<li style="background-image: url(images/carrouse_candy_tres.jpg);">
+			   	<li style="background-image: url(images/carrouse_candy_full.jpg);">
 			   		<div class="overlay"></div>
 			   		<div class="container">
 			   			<div class="row">
 				   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
 				   				<div class="slider-text-inner">
-				   					<h1><strong>Conoce lo que ofrecemos</strong></h1>
+				   					<h1><strong>Variedad de Productos que te hacen feliz</strong></h1>
 				   				</div>
 				   			</div>
 				   		</div>
@@ -60,6 +60,165 @@
 			  	</ul>
 		  	</div>
 		</aside>
+
+
+
+
+
+
+<section class="focus" id="focus">	
+	<div class="container">
+		<!-- SECTION HEADER -->
+		<div class="section-header">
+			<!-- SECTION TITLE AND SUBTITLE -->
+			<h2 class="dark-text">COFIKING</h2><div class="section-legend">Conoce lo que te ofrecemos</div>
+		</div>
+
+		<div class="row">
+			<span id="ctup-ads-widget-2" class="">
+				<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+					<a href="" class="service-icon">
+					    <span class="sr-only">Go to Gelatinas </span>								
+						<i class="pixeden" style="background:url(images/gelatina.png) no-repeat center;width:100%; height:100%;"></i>
+					</a>				
+					<h3 class="red-border-bottom">Gelatinas</h3>
+					<p>Con sabor a frutas</p>
+				</div>
+			</span><span id="ctup-ads-widget-1" class="">
+
+
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/gomitas/?customize_changeset_uuid=5a89d332-998b-4fd3-81bb-b83d604fc1a6" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Gomitas		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/gomitita.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Gomitas</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Ricas y divertidas</p>
+			</div>
+
+			</span><span id="ctup-ads-widget-3" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/chocolate/" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Chocolate		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/chocolatito.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Chocolate</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Con crema de avellanas</p>
+			</div>
+
+			</span><span id="ctup-ads-widget-8" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/chupetes/" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Chupetes		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/chupetito.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Chupetes</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Delicioso caramelo duro</p>
+			</div>
+
+			</span><span id="ctup-ads-widget-15" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/chicles/?customize_changeset_uuid=5a89d332-998b-4fd3-81bb-b83d604fc1a6" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Chicle		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/chiclesito.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Chicle</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Sabor a sandia </p>
+			</div>
+
+			</span><span id="ctup-ads-widget-6" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/caramelo-blando/?customize_changeset_uuid=5a89d332-998b-4fd3-81bb-b83d604fc1a6" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Caramelo Blando/Polvo		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/caramelito-blandito.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Caramelo Blando/Polvo</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Dulce y sabroso</p>
+			</div>
+
+			</span><span id="ctup-ads-widget-12" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/caramelo-liquido/?customize_changeset_uuid=5a89d332-998b-4fd3-81bb-b83d604fc1a6" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    CARAMELO LÍQUIDO		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/caramelito-liquido.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">CARAMELO LÍQUIDO</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Caramelo líquido de sabores</p>
+			</div>
+
+			</span><span id="ctup-ads-widget-14" class="">
+			<div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+
+										<a href="http://candyplanet.com.ec/index.php/categoria-producto/grageas/?customize_changeset_uuid=5a89d332-998b-4fd3-81bb-b83d604fc1a6" class="service-icon">
+															<span class="sr-only">
+			                    Go to			                    Grageas		                    </span>
+								
+							<i class="pixeden" style="background:url(http://candyplanet.com.ec/wp-content/uploads/2018/10/grageitas.png) no-repeat center;width:100%; height:100%;"></i>
+						</a>
+						
+
+				
+				<h3 class="red-border-bottom">Grageas</h3>
+				<!-- FOCUS HEADING -->
+
+				<p>Caramelo duro con centro ácido</p>
+			</div>
+
+
+	</div> <!-- / END CONTAINER -->
+
+	
+</section>  <!-- / END FOCUS SECTION -->
+
+
 
 	</body>
 		<?php 
