3459253f183e86aff482ec51ad093b08f4e9cd5e
StivZito <zito351@gmail.com>
2019-01-23 20:35:25 -0500

    carusel

--- homepage.php
+++ homepage.php
@@ -4,7 +4,65 @@
 		<?php require_once('layouts/header.php'); ?>
 	</head>
 	<body>
-
+		<aside id="qbootstrap-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/carrouse_candy_uno.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container">
+			   			<div class="row">
+				   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><strong> HACIENDO TU VIDA MAS DULCE</strong></h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/carrouse_candy_dos.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container">
+			   			<div class="row">
+				   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><strong>Líderes en confitería Ecuatoriana</strong></h1>
+										<p><h1><strong>Somos una empresa importadora de confitería comprometidos en ofrecer a nuestros clientes productos deliciosos, divertidos y de la más alta calidad. Con 15 años en el mercado hemos logrado posicionar nuestra marca en todo el país apoyándonos en la constante innovación, excelencia y transparencia, los que se han convertido en los principales pilares de ConfiKing.</h1></strong></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container">
+			   			<div class="row">
+				   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>Have an Access to <strong>a Health Professional at any time</strong></h1>
+										<h2>Free html5 templates Made by <a href="http://qbootstrap.com/" target="_blank">QBootstrap.com</a></h2>
+										<p><a class="btn btn-primary btn-lg btn-learn" href="appointment.html">Make an Appointment</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container">
+			   			<div class="row">
+				   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>Special offer! <strong>Free Consultation</strong> this month only</h1>
+										<h2>Free html5 templates Made by <a href="http://qbootstrap.com/" target="_blank">QBootstrap.com</a></h2>
+										<p><a class="btn btn-primary btn-lg btn-learn" href="appointment.html">Make an Appointment</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>		   	
+			  	</ul>
+		  	</div>
+		</aside>
 
 	</body>
 		<?php 
