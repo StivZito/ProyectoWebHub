1b044f853afce982c2d0667ddfdad1fbe2cfece5
StivZito <zito351@gmail.com>
2019-01-24 21:45:06 -0500

    Update login.php

--- login.php
+++ login.php
@@ -69,24 +69,15 @@
 		</div>
 	</div>
 	
-
 	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/bootstrap/js/popper.js"></script>
 	<script src="assets_log/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/daterangepicker/moment.min.js"></script>
 	<script src="assets_log/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
 	<script src="assets_log/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
